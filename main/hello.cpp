8a2c8bde3dda7a6e5edb1031ba12793ba09d37f3
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-03-08 06:47:25 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World i m PES2UG21CS031\n";
+  cout << "Hello, Jenkins\n";
+  cout<<"saying hi from webhooks\n";
+  cout << "I have successfully built and run\n";
+  cout<<"webhooks successfully enabled \n";
+  return 0;
+}
